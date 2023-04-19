fea4519dbba664470d7e1085bb308a2027780347
Mikołaj Szczepański <pm@mysy.pl>
2023-04-19 12:53:24 +0200

    Format functions.php

--- functions.php
+++ functions.php
@@ -3,49 +3,51 @@
 /**
  * Theme setup.
  */
-function develtio_setup() {
-	add_theme_support( 'title-tag' );
+function develtio_setup()
+{
+    add_theme_support('title-tag');
 
-	register_nav_menus(
-		array(
-			'primary' => __( 'Primary Menu', 'tailpress' ),
-		)
-	);
+    register_nav_menus(
+        array(
+            'primary' => __('Primary Menu', 'tailpress'),
+        )
+    );
 
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		)
-	);
+    add_theme_support(
+        'html5',
+        array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        )
+    );
 
-    add_theme_support( 'custom-logo' );
-	add_theme_support( 'post-thumbnails' );
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 
-	add_theme_support( 'align-wide' );
-	add_theme_support( 'wp-block-styles' );
+    add_theme_support('align-wide');
+    add_theme_support('wp-block-styles');
 
-	add_theme_support( 'editor-styles' );
-	add_editor_style( 'css/editor-style.css' );
+    add_theme_support('editor-styles');
+    add_editor_style('css/editor-style.css');
 }
 
-add_action( 'after_setup_theme', 'develtio_setup' );
+add_action('after_setup_theme', 'develtio_setup');
 
 /**
  * Enqueue theme assets.
  */
-function develtio_enqueue_scripts() {
-	$theme = wp_get_theme();
+function develtio_enqueue_scripts()
+{
+    $theme = wp_get_theme();
 
-	wp_enqueue_style( 'tailpress', develtio_asset( 'css/app.css' ), array(), $theme->get( 'Version' ) );
-	wp_enqueue_script( 'tailpress', develtio_asset( 'js/app.js' ), array(), $theme->get( 'Version' ) );
+    wp_enqueue_style('tailpress', develtio_asset('css/app.css'), array(), $theme->get('Version'));
+    wp_enqueue_script('tailpress', develtio_asset('js/app.js'), array(), $theme->get('Version'));
 }
 
-add_action( 'wp_enqueue_scripts', 'develtio_enqueue_scripts' );
+add_action('wp_enqueue_scripts', 'develtio_enqueue_scripts');
 
 /**
  * Get asset path.
@@ -54,12 +56,13 @@ add_action( 'wp_enqueue_scripts', 'develtio_enqueue_scripts' );
  *
  * @return string
  */
-function develtio_asset( $path ) {
-	if ( wp_get_environment_type() === 'production' ) {
-		return get_stylesheet_directory_uri() . '/' . $path;
-	}
+function develtio_asset($path)
+{
+    if (wp_get_environment_type() === 'production') {
+        return get_stylesheet_directory_uri() . '/' . $path;
+    }
 
-	return add_query_arg( 'time', time(),  get_stylesheet_directory_uri() . '/' . $path );
+    return add_query_arg('time', time(), get_stylesheet_directory_uri() . '/' . $path);
 }
 
 /**
@@ -71,19 +74,20 @@ function develtio_asset( $path ) {
  *
  * @return array
  */
-function develtio_nav_menu_add_li_class( $classes, $item, $args, $depth ) {
-	if ( isset( $args->li_class ) ) {
-		$classes[] = $args->li_class;
-	}
+function develtio_nav_menu_add_li_class($classes, $item, $args, $depth)
+{
+    if (isset($args->li_class)) {
+        $classes[] = $args->li_class;
+    }
 
-	if ( isset( $args->{"li_class_$depth"} ) ) {
-		$classes[] = $args->{"li_class_$depth"};
-	}
+    if (isset($args->{"li_class_$depth"})) {
+        $classes[] = $args->{"li_class_$depth"};
+    }
 
-	return $classes;
+    return $classes;
 }
 
-add_filter( 'nav_menu_css_class', 'develtio_nav_menu_add_li_class', 10, 4 );
+add_filter('nav_menu_css_class', 'develtio_nav_menu_add_li_class', 10, 4);
 
 /**
  * Adds option 'submenu_class' to 'wp_nav_menu'.
@@ -94,16 +98,17 @@ add_filter( 'nav_menu_css_class', 'develtio_nav_menu_add_li_class', 10, 4 );
  *
  * @return array
  */
-function develtio_nav_menu_add_submenu_class( $classes, $args, $depth ) {
-	if ( isset( $args->submenu_class ) ) {
-		$classes[] = $args->submenu_class;
-	}
+function develtio_nav_menu_add_submenu_class($classes, $args, $depth)
+{
+    if (isset($args->submenu_class)) {
+        $classes[] = $args->submenu_class;
+    }
 
-	if ( isset( $args->{"submenu_class_$depth"} ) ) {
-		$classes[] = $args->{"submenu_class_$depth"};
-	}
+    if (isset($args->{"submenu_class_$depth"})) {
+        $classes[] = $args->{"submenu_class_$depth"};
+    }
 
-	return $classes;
+    return $classes;
 }
 
-add_filter( 'nav_menu_submenu_css_class', 'develtio_nav_menu_add_submenu_class', 10, 3 );
+add_filter('nav_menu_submenu_css_class', 'develtio_nav_menu_add_submenu_class', 10, 3);
